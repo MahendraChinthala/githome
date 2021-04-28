8dc41d19265607957bd79ae4782d071de7f01964
DESKTOP-H0PC59H\MAHINDRA <mahimahendra603@gmail.com>
2021-04-28 13:28:05 +0530

    "added new things to home"

--- src/main/java/com/Controller/HomeController.java
+++ src/main/java/com/Controller/HomeController.java
@@ -2,6 +2,13 @@ package com.Controller;
 
 public class HomeController {
         public String hello() {
+            int i = 0;
+            int j =12;
+            if (i<j) {
+                System.out.println("Not Added Successfully");
+            }else {
+                System.out.println("added Successfully");
+            }
             return "Hello World!";
         }
     }
